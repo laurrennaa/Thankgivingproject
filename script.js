d6cb1571b9b53320f2983a0032964cd09987ace7
Lauren Larios <54549719+laurrennaa@users.noreply.github.com>
2019-12-09 10:19:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function changeImage1(){
+	document.getElementById("friends").src = "images/shows.jpg";
+}
+
+function changeImage2(){
+	document.getElementById("food").src = "images/pozole.jpg";
+}
+
+function changeImage3(){
+	document.getElementById("sleep").src = "images/coco.jpg";
+}
+
+function resetPage() {
+	location.reload();
+}
